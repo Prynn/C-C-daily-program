43a7ae5e2ee542c9497d89e66a66b84c19a249f2
Prynn <33374699+Prynn@users.noreply.github.com>
2017-12-13 11:18:21 +0800

    Create 对任意数量数排序.c

---
+++ 对任意数量数排序.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+void func(float a[],int n)
+{
+    int i,j;
+    float t;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<n-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+}
+
+int main()
+{
+   float grade[100];
+   int i=0,n=0;
+   while(scanf("%f",&grade[i])!=EOF)  重点在这
+   {
+       n++;i++;
+   }
+   func(grade,n);
+   for(i=n-1;i>=0;i--)
+   {
+       printf("%.1f ",grade[i]);
+   }
+    return 0;
+}
