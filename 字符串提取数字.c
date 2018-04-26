e59609a4edcd0156a2b82aa2bb43af947da505c7
Prynn <33374699+Prynn@users.noreply.github.com>
2018-04-26 21:11:29 +0800

    Update 字符串提取数字.c

--- 字符串提取数字.c
+++ 字符串提取数字.c
@@ -1,6 +1,6 @@
 这是C语言阶段考的一道题目，对于一串字符，提取其中的数字。
 要求：1.开头遇到空格要跳过。2.开头遇到'+'或'-'就当作符号。3.遇到非数字就结束。（看成了提取其中的数字，本题就是是简化了）
-
+1.用getchar（）逐个读取并实时处理。
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,5 +25,43 @@ int main()
         c=getchar();
     }
     printf("%d",s*n);
+    return 0;
+}
+2.用函数传指针形式处理
+#include <stdio.h>
+#include <stdlib.h>
+int func(char *s)
+{
+    int sum=0,f=1;
+    while(*s == ' ')
+    {
+        s++;
+    }
+    if(*s == '+')
+        f=1;
+    else if(*s == '-')
+        f=-1;
+    else if(*s<='9' && *s>='0')
+        sum=*s-'0';
+    else
+        return 0;
+    s++;
+    while(*s != '\0')
+    {
+        if(*s<='9' && *s>='0')
+            sum = sum*10 + (*s-'0');
+        else
+            break;
+        s++;
+    }
+    return sum*f;
+}
+int main()
+{
+    char a[50];  //如果定义成char* a;则出现错误；
+    while(scanf("%s",a)!=-1) //如果用gets()如何实现多组数据输入?
+    {
+        printf("%d\n",func(a));
+    }
     return 0;
 }
